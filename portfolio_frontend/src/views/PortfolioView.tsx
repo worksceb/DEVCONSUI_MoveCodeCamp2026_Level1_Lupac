85fc795fcbdbfc9623a092b685ddf4ec7471ad8d
worksceb <cebastianworks@gmail.com>
2026-03-28 16:19:44 +0800

    hey

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x97b34e0baf400345752e8352731e9539cd75ca1d44c86cce802d1e3ca118df15";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
